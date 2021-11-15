0376b66309ac4f9bfc1020bf7ce2c9cd89cbab94
Will Aitken <will.aitken1@outlook.com>
2021-11-15 09:26:22 -0500

    Imported parse_value instead of copying

--- blink_test.py
+++ blink_test.py
@@ -1,25 +1,8 @@
-from mbientlab.metawear import MetaWear, libmetawear
+from mbientlab.metawear import MetaWear, libmetawear, parse_value
 from mbientlab.metawear.cbindings import *
 import time
 import matplotlib.pyplot as plt
 
-_value_parsers = {
-    DataTypeId.UINT32: lambda p: cast(p.contents.value, POINTER(c_uint)).contents.value,
-    DataTypeId.INT32: lambda p: cast(p.contents.value, POINTER(c_int)).contents.value,
-    DataTypeId.FLOAT: lambda p: cast(p.contents.value, POINTER(c_float)).contents.value,
-    DataTypeId.CARTESIAN_FLOAT: lambda p: cast(p.contents.value, POINTER(CartesianFloat)).contents,
-    DataTypeId.BATTERY_STATE: lambda p: cast(p.contents.value, POINTER(BatteryState)).contents,
-    DataTypeId.TCS34725_ADC: lambda p: cast(p.contents.value, POINTER(Tcs34725ColorAdc)).contents,
-    DataTypeId.EULER_ANGLE: lambda p: cast(p.contents.value, POINTER(EulerAngles)).contents,
-    DataTypeId.QUATERNION: lambda p: cast(p.contents.value, POINTER(Quaternion)).contents,
-    DataTypeId.CORRECTED_CARTESIAN_FLOAT: lambda p: cast(p.contents.value, POINTER(CorrectedCartesianFloat)).contents,
-    DataTypeId.OVERFLOW_STATE: lambda p: cast(p.contents.value, POINTER(OverflowState)).contents,
-    DataTypeId.LOGGING_TIME: lambda p: cast(p.contents.value, POINTER(LoggingTime)).contents,
-    DataTypeId.BTLE_ADDRESS: lambda p: cast(p.contents.value, POINTER(BtleAddress)).contents,
-    DataTypeId.BOSCH_ANY_MOTION: lambda p: cast(p.contents.value, POINTER(BoschAnyMotion)).contents,
-    DataTypeId.CALIBRATION_STATE: lambda p: cast(p.contents.value, POINTER(CalibrationState)).contents,
-    DataTypeId.BOSCH_TAP: lambda p: cast(p.contents.value, POINTER(BoschTap)).contents
-}
 
 def main():
 
@@ -30,7 +13,8 @@ def main():
 
     pattern = LedPattern(repeat_count=Const.LED_REPEAT_INDEFINITELY)
     libmetawear.mbl_mw_led_load_preset_pattern(byref(pattern), LedPreset.SOLID)
-    libmetawear.mbl_mw_led_write_pattern(device.board, byref(pattern), LedColor.GREEN)
+    libmetawear.mbl_mw_led_write_pattern(device.board, 
+         byref(pattern), LedColor.GREEN)
     libmetawear.mbl_mw_led_play(device.board)
 
     time.sleep(5.0)
@@ -65,41 +49,46 @@ class test():
         callback     = FnVoid_VoidP_DataP(self.data_handler)
 
         # Setup the accelerometer sample frequency and range
-        #libmetawear.mbl_mw_acc_set_odr(device.board, 100.0)
-        #libmetawear.mbl_mw_acc_set_range(device.board, 16.0)
-        #libmetawear.mbl_mw_acc_write_acceleration_config(device.board)
+        libmetawear.mbl_mw_acc_set_odr(device.board, 100.0)
+        libmetawear.mbl_mw_acc_set_range(device.board, 16.0)
+        libmetawear.mbl_mw_acc_write_acceleration_config(device.board)
 
         # Get the accelerometer data signal
-        #signal = libmetawear.mbl_mw_acc_get_acceleration_data_signal(device.board)
+        signal = libmetawear.mbl_mw_acc_get_acceleration_data_signal(device.board)
         # Subscribe to it
-        #libmetawear.mbl_mw_datasignal_subscribe(signal, None, callback_acc)
+        libmetawear.mbl_mw_datasignal_subscribe(signal, None, callback_acc)
 
         # Enable the accelerometer
-        #libmetawear.mbl_mw_acc_enable_acceleration_sampling(device.board)
-        #libmetawear.mbl_mw_acc_start(device.board)
+        libmetawear.mbl_mw_acc_enable_acceleration_sampling(device.board)
+        libmetawear.mbl_mw_acc_start(device.board)
 
         # Get the gyroscope data signal
-        # signal = libmetawear.mbl_mw_gyro_bmi270_get_packed_rotation_data_signal(device.board)
+        signal = libmetawear.mbl_mw_gyro_bmi270_get_packed_rotation_data_signal(device.board)
         # Subscribe to it
-        # libmetawear.mbl_mw_datasignal_subscribe(signal, None, callback_gyr)
+        libmetawear.mbl_mw_datasignal_subscribe(signal, None, callback_gyr)
 
         # Enable the gyroscope
-        # libmetawear.mbl_mw_gyro_bmi270_enable_rotation_sampling(device.board)
-        #libmetawear.mbl_mw_gyro_bmi270_start(device.board)
+        libmetawear.mbl_mw_gyro_bmi270_enable_rotation_sampling(device.board)
+        libmetawear.mbl_mw_gyro_bmi270_start(device.board)
 
         # Sensor fusion setup
-        libmetawear.mbl_mw_sensor_fusion_set_mode(device.board, SensorFusionMode.IMU_PLUS);
-        libmetawear.mbl_mw_sensor_fusion_set_acc_range(device.board, SensorFusionAccRange._8G);
-        libmetawear.mbl_mw_sensor_fusion_set_gyro_range(device.board, SensorFusionGyroRange._2000DPS);
+        libmetawear.mbl_mw_sensor_fusion_set_mode(device.board, 
+                SensorFusionMode.IMU_PLUS);
+        libmetawear.mbl_mw_sensor_fusion_set_acc_range(device.board, 
+                SensorFusionAccRange._8G);
+        libmetawear.mbl_mw_sensor_fusion_set_gyro_range(device.board, 
+                SensorFusionGyroRange._2000DPS);
         libmetawear.mbl_mw_sensor_fusion_write_config(device.board);
 
         # Subscribe to the quaternion signal
-        signal = libmetawear.mbl_mw_sensor_fusion_get_data_signal(device.board, SensorFusionData.QUATERNION);
+        signal = libmetawear.mbl_mw_sensor_fusion_get_data_signal(device.board, 
+                SensorFusionData.QUATERNION);
         libmetawear.mbl_mw_datasignal_subscribe(signal, None, callback);
 
 
         # Start sensor fusion (acc + gyro + mag + on-board sensor fusion algo)
-        libmetawear.mbl_mw_sensor_fusion_enable_data(device.board, SensorFusionData.QUATERNION);
+        libmetawear.mbl_mw_sensor_fusion_enable_data(device.board, 
+                SensorFusionData.QUATERNION);
         libmetawear.mbl_mw_sensor_fusion_start(device.board);
 
         # Start time to track data
@@ -117,26 +106,27 @@ class test():
         libmetawear.mbl_mw_sensor_fusion_stop(device.board);
 
         # Unsubscribe
-        signal = libmetawear.mbl_mw_sensor_fusion_get_data_signal(device.board, SensorFusionData.QUATERNION);
+        signal = libmetawear.mbl_mw_sensor_fusion_get_data_signal(device.board, 
+                SensorFusionData.QUATERNION);
         libmetawear.mbl_mw_datasignal_unsubscribe(signal);
 
         # Disable the gyroscope
-        #libmetawear.mbl_mw_gyro_bmi160_stop(device.board)
-        #libmetawear.mbl_mw_gyro_bmi160_disable_rotation_sampling(device.board)
+        libmetawear.mbl_mw_gyro_bmi160_stop(device.board)
+        libmetawear.mbl_mw_gyro_bmi160_disable_rotation_sampling(device.board)
 
         # Unsubscribe to it
-        #libmetawear.mbl_mw_datasignal_unsubscribe(signal)
-        #libmetawear.mbl_mw_debug_disconnect(device.board)
+        libmetawear.mbl_mw_datasignal_unsubscribe(signal)
+        libmetawear.mbl_mw_debug_disconnect(device.board)
 
 
         # Disable the accelerometer
-        #libmetawear.mbl_mw_acc_stop(device.board)
-        #libmetawear.mbl_mw_acc_disable_acceleration_sampling(device.board)
+        libmetawear.mbl_mw_acc_stop(device.board)
+        libmetawear.mbl_mw_acc_disable_acceleration_sampling(device.board)
 
         # Unsubscribe to it
-        #signal = libmetawear.mbl_mw_acc_get_acceleration_data_signal(device.board)
-        #libmetawear.mbl_mw_datasignal_unsubscribe(signal)
-        #libmetawear.mbl_mw_debug_disconnect(device.board)
+        signal = libmetawear.mbl_mw_acc_get_acceleration_data_signal(device.board)
+        libmetawear.mbl_mw_datasignal_unsubscribe(signal)
+        libmetawear.mbl_mw_debug_disconnect(device.board)
 
         figs, axs = plt.subplots(1,2)
         axs[0].plot(self.time_acc,self.x_acc, label = "X")
@@ -156,14 +146,16 @@ class test():
 
     # Callback function to process/parse the gyroscope data
     def data_handler_acc(self, ctx, data):
-        #print("%s    : %s -> %s" % ("Accelrometer", self.device.address, parse_value(data)))
+        #print("%s    : %s -> %s" % ("Accelrometer" \
+        #    self.device.address, parse_value(data)))
         self. x_acc.append(parse_value(data).x)
         self.y_acc.append(parse_value(data).y)
         self.z_acc.append(parse_value(data).z)
         self.time_acc.append(time.time()-self.start_time)
 
     def data_handler_gyr(self, ctx, data):
-        #print("%s    : %s -> %s" % ("Gyroscope", self.device.address, parse_value(data)))
+        #print("%s    : %s -> %s" % ("Gyroscope", \
+        #    self.device.address, parse_value(data)))
         self. x_gyr.append(parse_value(data).x)
         self.y_gyr.append(parse_value(data).y)
         self.z_gyr.append(parse_value(data).z)
@@ -171,32 +163,6 @@ class test():
     
     def data_handler (self, ctx, data):
         print("%s    : %s -> %s" % ("New", self.device.address, parse_value(data)))
-
-def parse_value(pointer, **kwargs):
-        """
-        Helper function to extract the value from a Data object.  If you are storing the values to be used at a later time,
-        call copy.deepcopy preserve the value.  You do not need to do this if the underlying type is a native type or a byte array
-        @params:
-            pointer     - Required  : Pointer to a Data object
-            n_elem      - Optional  : Nummber of elements in the value array if the type_id attribute is DataTypeId.DATA_ARRAY
-        """
-        if (pointer.contents.type_id in _value_parsers):
-            return _value_parsers[pointer.contents.type_id](pointer)
-        elif (pointer.contents.type_id == DataTypeId.SENSOR_ORIENTATION):
-            return _value_parsers[DataTypeId.INT32](pointer)
-        elif (pointer.contents.type_id == DataTypeId.BYTE_ARRAY):
-            array_ptr= cast(pointer.contents.value, POINTER(c_ubyte * pointer.contents.length))
-            return [array_ptr.contents[i] for i in range(0, pointer.contents.length)]
-        elif (pointer.contents.type_id == DataTypeId.DATA_ARRAY):
-            if 'n_elem' in kwargs:
-                values = cast(pointer.contents.value, POINTER(POINTER(Data) * kwargs['n_elem']))
-                return [parse_value(values.contents[i]) for i in range(0, kwargs['n_elem'])]
-            else:
-                raise RuntimeError("Missing optional parameter 'n_elem' for parsing DataTypeId.DATA_ARRAY value")
-        else:
-            raise RuntimeError('Unrecognized data type id: ' + str(pointer.contents.type_id))
-
-
 
 
 if __name__ == "__main__":
