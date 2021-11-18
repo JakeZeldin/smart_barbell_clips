b0f3f0e2f30ee0b888b09a5cae76a8eab96caceb
Will Aitken <will.aitken1@outlook.com>
2021-11-18 15:37:44 -0500

    Wonky, but plotting sort of

--- sensor_fusion.py
+++ sensor_fusion.py
@@ -2,13 +2,14 @@ from mbientlab.metawear import MetaWear, libmetawear, parse_value
 from mbientlab.metawear.cbindings import *
 import time
 import matplotlib.pyplot as plt
-import scipy
+import scipy.integrate
+from mpl_toolkits.mplot3d import Axes3D
 
 
 def main():
 
-    address = "C7:EA:21:57:F5:E2" #Will
-    #address = "C4:A3:A4:75:A2:86" #Matt
+    #address = "C7:EA:21:57:F5:E2" #Will
+    address = "C4:A3:A4:75:A2:86" #Matt
 
     device = MetaWear(address)
     device.connect()
@@ -17,26 +18,59 @@ def main():
 
     session.startup()
     print("Recording")
-    time.sleep(2)
+    time.sleep(5)
     print("Stopped")
     session.shutdown()
 
-    for la in session.lin_acc:
-        print(la)
+    acc_x = [acc[0]*9.81 for acc in session.lin_acc]
+    acc_y = [acc[1]*9.81 for acc in session.lin_acc]
+    acc_z = [(acc[2]*9.81) for acc in session.lin_acc]
 
-    acc_x = [acc[0] for acc in session.lin_acc]
-    acc_y = [acc[1] for acc in session.lin_acc]
-    acc_z = [acc[2] for acc in session.lin_acc]
+    for i in range(len(acc_z)):
+        if (abs(acc_x[i]) < 0.2):
+            acc_x[i] = 0
+        if (abs(acc_y[i]) < 0.2):
+            acc_y[i] = 0
+        if (abs(acc_z[i]) < 0.2):
+            acc_z[i] = 0
+        print(acc_x[i], acc_y[i], acc_z[i])
 
-    vel_x = scipy.integrate.cumulative_trapezoid(acc_x)
-    vel_y = scipy.integrate.cumulative_trapezoid(acc_y)
-    vel_z = scipy.integrate.cumulative_trapezoid(acc_z)
 
-    pos_x = scipy.integrate.cumulative_trapezoid(vel_x)
-    pos_y = scipy.integrate.cumulative_trapezoid(vel_y)
-    pos_z = scipy.integrate.cumulative_trapezoid(vel_z)
+    vel_x = [0] 
+    vel_y = [0] 
+    vel_z = [0] 
+    for i in range (1, len(acc_x) - 1):
+        vel_x.append(vel_x[i-1] + acc_x[i]*0.01)
+        vel_y.append(vel_y[i-1] + acc_y[i]*0.01)
+        vel_z.append(vel_z[i-1] + acc_z[i]*0.01)
 
-    print(pos_x)
+    # vel_x = scipy.integrate.cumulative_trapezoid(acc_x)
+    # vel_y = scipy.integrate.cumulative_trapezoid(acc_y)
+    # vel_z = scipy.integrate.cumulative_trapezoid(acc_z)
+
+    pos_x = [0] 
+    pos_y = [0] 
+    pos_z = [0] 
+    for i in range (1, len(vel_x) - 1):
+        pos_x.append(pos_x[i-1] + vel_x[i]*0.01)
+        pos_y.append(pos_y[i-1] + vel_y[i]*0.01)
+        pos_z.append(pos_z[i-1] + vel_z[i]*0.01)
+
+    # pos_x = scipy.integrate.cumulative_trapezoid(vel_x)
+    # pos_y = scipy.integrate.cumulative_trapezoid(vel_y)
+    # pos_z = scipy.integrate.cumulative_trapezoid(vel_z)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.set_xlim3d(min([min(pos_x), min(pos_y), min(pos_z)]), max([max(pos_x),
+        max(pos_y), max(pos_z)]))
+    ax.set_ylim3d(min([min(pos_x), min(pos_y), min(pos_z)]), max([max(pos_x),
+        max(pos_y), max(pos_z)]))
+    ax.set_zlim3d(min([min(pos_x), min(pos_y), min(pos_z)]), max([max(pos_x),
+        max(pos_y), max(pos_z)]))
+    ax.plot(pos_x, pos_y, pos_z)
+    plt.show()
+    Axes3D.plot()
 
     device.on_disconnect = lambda status: print("disconnected")
     device.disconnect()
@@ -50,9 +84,24 @@ class State():
         self.callback = FnVoid_VoidP_DataP(self.data_handler)
 
     def startup(self):
+
+        # # Setup the accelerometer sample frequency and range
+        # libmetawear.mbl_mw_acc_set_odr(self.device.board, 100.0)
+        # libmetawear.mbl_mw_acc_set_range(self.device.board, 16.0)
+        # libmetawear.mbl_mw_acc_write_acceleration_config(self.device.board)
+
+        # # Get the accelerometer data signal
+        # signal = libmetawear.mbl_mw_acc_get_acceleration_data_signal(self.device.board)
+        # # Subscribe to it
+        # libmetawear.mbl_mw_datasignal_subscribe(signal, None, self.callback)
+
+        # # Enable the accelerometer
+        # libmetawear.mbl_mw_acc_enable_acceleration_sampling(self.device.board)
+        # libmetawear.mbl_mw_acc_start(self.device.board)
+
         # Sensor fusion setup
         libmetawear.mbl_mw_sensor_fusion_set_mode(self.device.board, 
-                SensorFusionMode.NDOF);
+                SensorFusionMode.IMU_PLUS);
         libmetawear.mbl_mw_sensor_fusion_set_acc_range(self.device.board, 
                 SensorFusionAccRange._8G);
         libmetawear.mbl_mw_sensor_fusion_set_gyro_range(self.device.board, 
