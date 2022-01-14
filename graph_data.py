5edda1e1aa34bed49b922ea3e5dc1cd09c896219
Matt <matthewglass11@gmail.com>
2022-01-14 14:19:32 -0500

    Added code to have 2 sensors going

--- graph_data.py
+++ graph_data.py
@@ -10,40 +10,98 @@ import sys, getopt
 from mpl_toolkits.mplot3d import Axes3D
 from scipy import signal
 
-def function(do_acc, do_gyro):
-    # which sensor are we using
-    #address = "C7:EA:21:57:F5:E2" #Will
-    address = "C4:A3:A4:75:A2:86" #Matt
-
-    device = MetaWear(address)
-    device.connect()
-    session = State(device)
-    if do_acc == 1:
-        session.startup_accelerometer()
-    if do_gyro == 1:
-        session.startup_gyroscope()
-    if do_acc == 1:
-        session.enable_accelerometer()
-    if do_gyro == 1:
-        session.enable_gyroscope()
+def function(do_acc, do_gyro,m,w, both_sens):
+    #which sensor are we using
+    if both_sens == 0 and w == 1:
+        address = "C7:EA:21:57:F5:E2" #Will
+    elif both_sens ==0 and m == 1:
+        address = "C4:A3:A4:75:A2:86" #Matt
+    else:
+        address1 = "C4:A3:A4:75:A2:86" #Matt
+        address2 = "C7:EA:21:57:F5:E2" #Will
     
-    print("Recording")
-    time.sleep(5) #how long the sensor will record for
-    print("Stopped")
-    if do_acc == 1:
-        session.shutdown_accelerometer()
-    if do_gyro == 1:
-        session.shutdown_gyroscope()
+    if both_sens == 0:
+        device = MetaWear(address)
+        device.connect()
+        session = State(device)
+        if do_acc == 1:
+            session.startup_accelerometer()
+        if do_gyro == 1:
+            session.startup_gyroscope()
+        if do_acc == 1:
+            session.enable_accelerometer()
+        if do_gyro == 1:
+            session.enable_gyroscope()
+    
+        print("Recording")
+        time.sleep(5) #how long the sensor will record for
+        print("Stopped")
+        if do_acc == 1:
+            session.shutdown_accelerometer()
+        if do_gyro == 1:
+            session.shutdown_gyroscope()
 
-    device.disconnect()
+        device.disconnect()
 
+        if do_acc == 1 and do_gyro == 1:
+            if m ==1:
+                session.plot_acc_gyr(1)
+            else:
+                session.plot_acc_gyr(2)
+        elif do_acc == 1:
+            if m == 1:
+                session.plot_acc(1)
+            else:
+                session.plot_acc(2)
+        elif do_gyro == 1:
+            if w == 1:
+                session.plot_gyr(1)
+            else:
+                session.plot_gyr(2)
+    
+    else:
+        device1 = MetaWear(address)
+        device2 = MetaWear(address)
+        device1.connect()
+        device2.connect()
+        session1 = State(device1)
+        session2 = State(device2)
+        if do_acc == 1:
+            session1.startup_accelerometer()
+            session2.startup_accelerometer()
+        if do_gyro == 1:
+            session1.startup_gyroscope()
+            session2.startup_gyroscope()
+        if do_acc == 1:
+            session1.enable_accelerometer()
+            session2.enable_accelerometer()
+        if do_gyro == 1:
+            session1.enable_gyroscope()
+            session2.enable_gyroscope()
 
-    if do_acc == 1 and do_gyro == 1:
-        session.plot_acc_gyr()
-    elif do_acc == 1:
-        session.plot_acc()
-    elif do_gyro == 1:
-        session.plot_gyr()
+        print("Recording")
+        time.sleep(5) #how long the sensor will record for
+        print("Stopped")
+        if do_acc == 1:
+            session1.shutdown_accelerometer()
+            session2.shutdown_accelerometer()
+        if do_gyro == 1:
+            session1.shutdown_gyroscope()
+            session2.shutdown_gyroscope()
+
+        device1.disconnect()
+        device2.disconnect()
+
+        if do_acc == 1 and do_gyro == 1:
+            session1.plot_acc_gyr(1)
+            session2.plot_acc_gyr(2)
+        elif do_acc == 1:
+            session1.plot_acc(1)
+            session2.plot_acc(2)
+        elif do_gyro == 1:
+            session1.plot_gyr(1)
+            session2.plot_gyr(2)
+    
 
 class State():
     def __init__(self, device):
@@ -124,36 +182,47 @@ class State():
         self.gyr_z.append(d.z)
         self.gyr_samples += 1
 
-    def plot_acc (self):
-        
+    def plot_acc (self, num):
+        if num == 1:
+            sensor_name = "C4"
+        else:
+            sensor_name = "C7"
         plt.plot(self.acc_x, label = "X")
         plt.plot(self.acc_y, label = "Y")
         plt.plot(self.acc_z, label = "Z")
-        plt.title('Acc')
+        plt.title('Acc ' + sensor_name)
         plt.legend()
         plt.show()
 
-    def plot_gyr (self):
+    def plot_gyr (self, num):
+        if num == 1:
+            sensor_name = "C4"
+        else:
+            sensor_name = "C7"
         plt.plot(self.gyr_x, label = "X")
         plt.plot(self.gyr_y, label = "Y")
         plt.plot( self.gyr_z, label = "Z")
-        plt.title('Gyr')
+        plt.title('Gyr ' + sensor_name)
         plt.legend()
         plt.show()
 
-    def plot_acc_gyr(self):
+    def plot_acc_gyr(self, num):
+        if num == 1:
+            sensor_name = "C4"
+        else:
+            sensor_name = "C7"
 
         figs, axs = plt.subplots(1,2)
         axs[0].plot(self.acc_x, label = "X")
         axs[0].plot(self.acc_y, label = "Y")
         axs[0].plot(self.acc_z, label = "Z")
-        axs[0].set_title('Acc')
+        axs[0].set_title('Acc ' + sensor_name)
         axs[0].legend()
 
         axs[1].plot(self.gyr_x, label = "X")
         axs[1].plot(self.gyr_y, label = "Y")
         axs[1].plot(self.gyr_z, label = "Z")
-        axs[1].set_title('Gyr')
+        axs[1].set_title('Gyr ' + sensor_name)
         axs[1].legend()
         
         plt.show()
@@ -161,9 +230,11 @@ class State():
 def main(argv):    
     do_acc = 0
     do_gyro = 0
-    
+    m = 0
+    w = 0
+    both_sens = 0
     try:
-        opts, args = getopt.getopt(argv,"ag",["acc","gyr"])
+        opts, args = getopt.getopt(argv,"agmw",["acc","gyr"])
     except getopt.GetoptError:
         print ("error FUCK")
         sys.exit(2)
@@ -174,9 +245,20 @@ def main(argv):
         elif opt in ("-g", "--gyr"):
             print ('Finna do gyroscope')
             do_gyro = 1
-
-    print(do_acc,do_gyro)
-    function(do_acc, do_gyro)
+        elif opt == "-m":
+            m = 1
+        elif opt == "-w":
+            w = 1
+    
+    if m == 1 and w == 1:
+        both_sens = 1
+    
+    if m ==0 and w == 0:
+        print("No sensor selected use -m(c4) or -w(c7) to select which sensor your using")
+        sys.exit(2)
+    
+    
+    function(do_acc, do_gyro, m, w, both_sens)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
